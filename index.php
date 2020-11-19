c596276087d34366ae56a03d24b981b2ec0f3b27
Dorian Storella <dorianstorella@gmail.com>
2020-11-19 16:04:55 +0100

    correction

--- index.php
+++ index.php
@@ -11,19 +11,20 @@
 </head>
 <body>
 <header>           
-    <!--Main Navigation-->
 <!--Navbar-->
-<nav class="navbar navbar-light navbar-1 white">
+
+<nav class="navbar navbar-light purple lighten-4 mb-4">
 
   <!-- Navbar brand -->
-  <a class="navbar-brand" href="#">ImmoEliza</a>
+  <a class="navbar-brand" href="#">Navbar</a>
 
   <!-- Collapse button -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15"
-    aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+  <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
+    aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i
+        class="fas fa-bars fa-1x"></i></span></button>
 
   <!-- Collapsible content -->
-  <div class="collapse navbar-collapse" id="navbarSupportedContent15">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent1">
 
     <!-- Links -->
     <ul class="navbar-nav mr-auto">
@@ -33,9 +34,7 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Information</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#"></a>
-      </li>
+      
     </ul>
     <!-- Links -->
 
@@ -64,5 +63,8 @@
 
 </footer>
     <script src="./assets/js/script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>    
 </body>
 </html>
